fb72c6035290fe349e48a4905669bd37a0ce06d3
Leonard M Reidy <leonard.reidy@gmail.com>
2023-12-25 09:48:07 +0000

    Troubleshoot networking issues

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,8 @@ require('dotenv').config()
 import { app } from "./app";
 import { logger } from "./logger/logger";
 import { ServerPresenter } from "./server/presenters/server.presenter";
-import { RANDOM_PORT } from "./server/constants/server.constants";
 
-const server = app.listen(process.env.PORT ?? RANDOM_PORT, () => {
+const server = app.listen(process.env.PORT ?? 7108, () => {
   const serverProperties = new ServerPresenter(server);
   logger.info(`Server listening @ ${serverProperties.address}`);
 });
